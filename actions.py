82602fa8524ec84f82282a05d2395d20ad22e6a8
pri021 <90155944+pri021@users.noreply.github.com>
2022-06-17 17:54:42 +0530

    Update actions.py

--- actions.py
+++ actions.py
@@ -1,12 +1,19 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
-from weather import Weather
+import requests 
 
 
-class ActionHelloWorld(Action):
+
+class ActionCityWeather(Action):
 
     def name(self) -> Text:
         return "action_city_weather"
@@ -19,7 +26,23 @@ class ActionHelloWorld(Action):
         response = "The current temperature at {} is {} degree Celsius.".format(city,temperature)
         dispatcher.utter_message(response)
 
-        return [SlotSet('location',city)]
+        return []
+
+
+def Weather(city):
+    API_key = "12c2be6f00d60769df0dace1036caa95"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    
+    Final_url = base_url + "appid=" + API_key + "&q=" + city + "&units=metric"
+    
+    response = requests.get(base_url.format(city))
+    if response.status_code == 404:
+        
+        weather_data = response.json()
+        
+        return weather_data['main']
+
+# This is a simple example for a custom action which utters "Hello World!"
 
 # from typing import Any, Text, Dict, List
 #
