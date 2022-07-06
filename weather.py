7a1ccc5d445667b9554a9b5efef93c3b4d152109
pri021 <90155944+pri021@users.noreply.github.com>
2022-07-06 14:36:34 +0530

    Delete weather.py

--- weather.py
+++
@@ -1,9 +0,0 @@
-import requests
-def Weather(city):
-    API_key = "http://api.openweathermap.org/data/2.5/weather?appid=12c2be6f00d60769df0dace1036caa95="
-    base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    
-    Final_url = base_url + "appid=" + API_key + "&q=" + city + "&units=metric"
-    weather_data = requests.get(Final_url).json()
-    
-    return weather_data['main']
